1b689af97e39de8ba1173ff211a133a3e183bb0e
swapcy <email2swap@gmail.com>
2018-08-09 08:50:42 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,16 +1,23 @@
+# %load q01_plot_corr/build.py
 # Default imports
 import pandas as pd
-from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
-plt.switch_backend('agg')
+#from matplotlib.pyplot import yticks, xticks, subplots, set_cmap 
+import matplotlib.pyplot as plt
+import seaborn as sns
+#plt.switch_backend('agg')
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your solution here:
 def plot_corr(data, size=11):
     corr = data.corr()
-    fig, ax = subplots(figsize=(size, size))
-    set_cmap("YlOrRd")
+    fig, ax = plt.subplots(figsize=(size, size))
+    plt.set_cmap('YlOrRd')
     ax.matshow(corr)
-    xticks(range(len(corr.columns)), corr.columns, rotation=90)
-    yticks(range(len(corr.columns)), corr.columns)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    
     return ax
+
+#plot_corr(data)
+
